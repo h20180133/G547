0c2eb8fcc6e7d2a8b264b51d0adf049c1d7948c6
h20180133 <47377372+h20180133@users.noreply.github.com>
2019-04-29 00:02:58 +0530

    Add files via upload

--- Project/userapp/commands.h
+++ Project/userapp/commands.h
@@ -2,14 +2,12 @@
 #ifndef _COMMANDS_H
 #define _COMMANDS_H
 
-#define EP_OUT 0x00
 
 #define MAGIC_NUMBER 'T'
 #define ADC_SEQ 0x01
 
 
 #define THRESHOLD _IOW(MAGIC_NUMBER,ADC_SEQ,unsigned long)
-
 
 
 #endif
